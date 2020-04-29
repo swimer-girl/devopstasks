988f3769e8d4b70dea507159a16c977c990b9553
swimer-girl <swimermail@gmail.com>
2020-04-29 20:13:46 +0300

    Исправление задания по модулю3
    Исправления по заданию multiple_in_range

--- python/module3/multiple_in_range.py
+++ python/module3/multiple_in_range.py
@@ -1,5 +1,3 @@
 def multiple_in_range(start, stop):
-    for i in range(start, stop + 1):
-        if i % 7 == 0 and i % 5 != 0:
-            return [i]
+    return [i for i in range(start, stop+1) if (i % 7 == 0) and (i % 5 != 0)]
 
